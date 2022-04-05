cabed870e247fd65c015be88d6101ef7ce735e7f
Trinidad <trinidadsaldarriaga23@gmail.com>
2022-04-05 09:53:58 -0300

    agrego archivo de texto

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,15 @@ using namespace std;
 
 int main(void){
 
-    cout<<"Hola"<<endl;
+    char buf[100];
+    FILE*fd;
+
     cout<<"========Inicio del programa=========="<<endl;
+    fd = fopen("archivo_texto.txt","r+t");
+    fread(buf,100,1,fd);
+    printf("%s",buf);
+    fclose(fd);
+    cout<<" "<<endl;
+    cout<<"=========Fin del programa========="<<endl;
+
 }
