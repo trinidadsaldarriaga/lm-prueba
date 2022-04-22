fa47fdfae2d1019118a6373c047682b93fc2a76f
trinidadsaldarriaga <66488177+trinidadsaldarriaga@users.noreply.github.com>
2022-04-21 23:37:33 -0300

    funcion inline (#8)

--- src/matematica.h
+++ src/matematica.h
@@ -3,3 +3,12 @@
 #include <stdint.h>
 int32_t Sumar_Array(int16_t *x, int16_t xn);
 int16_t Multiplicar_Sat (int16_t n1, int16_t n2);
+inline int16_t Multip_Sat (int16_t n1, int16_t n2){
+    int16_t n3 = n1*n2;
+    if (n3<n1){
+        return -1; //overflow
+    }
+    else{
+        return n3;
+    }
+}
