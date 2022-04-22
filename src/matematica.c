67837966144c9b0e9a4825976d9ede47818447c8
Trinidad <trinidadsaldarriaga23@gmail.com>
2022-04-21 22:23:05 -0300

    ejercicio3

--- src/matematica.c
+++ src/matematica.c
@@ -3,8 +3,8 @@
 #include <stdint.h>
 #include "matematica.h"
 
-int Sumar_Array (int* x, int xn){
-    int suma = 0;
+int32_t Sumar_Array (int* x, int xn){
+    int16_t suma = 0;
     for(int i = 0; i<xn; i++)
     {
         suma = suma + *x; //voy sumando los elementos del array
@@ -15,4 +15,11 @@ int Sumar_Array (int* x, int xn){
     }
     printf("La suma del array es %d",suma);
     return suma;
+}
+
+int16_t Multiplicar_Sat (int16_t n1, int16_t n2)
+{
+    int16_t n3 = n1*n2;
+    printf("La multiplicacion da: %d",n3);
+    return n3;
 }
